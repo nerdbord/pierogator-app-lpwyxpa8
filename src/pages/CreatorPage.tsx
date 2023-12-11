886a1aa7458e197989302b92b1449f0beb5d3818
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2023-12-11 04:31:35 +0100

    added BarButton component

--- src/pages/CreatorPage.tsx
+++ src/pages/CreatorPage.tsx
@@ -1,8 +1,13 @@
 import GeneratorForm from '@components/Form/GeneratorForm';
 import Button from '@components/Button';
+import BarButton from '@components/BarButton/BarButton';
 
 const print = () => {
   console.log('click');
+};
+
+const print2 = () => {
+  console.log('Zapisz');
 };
 
 const CreatorPage = () => {
@@ -10,6 +15,10 @@ const CreatorPage = () => {
     <>
       <Button onClick={print} text="Generuj" />
       <GeneratorForm />
+      <BarButton
+        onClick={print2}
+        text="Zapisz i przejdź do tworzenia przepisu"
+      />
     </>
   );
 };
