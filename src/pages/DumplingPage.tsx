282bc59a1b760470df9898f022929ace889ebbc2
KamWit7 <kamilw77@interia.pl>
2023-12-11 20:01:07 +0100

    dumplings to show

--- src/pages/DumplingPage.tsx
+++ src/pages/DumplingPage.tsx
@@ -1,8 +1,9 @@
+
 import { useParams } from 'react-router-dom';
 
 const DumplingPage = () => {
   const { id } = useParams();
-
+  
   return (
     <div>
       <h1>Pieróg</h1>
--- src/pages/DumplingShopPage.tsx
+++ src/pages/DumplingShopPage.tsx
@@ -1,4 +1,64 @@
+import { findMyDumplingRecipes, listAllDumplingRecipes } from '@src/API';
+import { useEffect, useState } from 'react';
+
+type DumplingType = {
+  status: 'fulfilled' | 'rejected';
+  value:
+    | { name: string; imageSrc: string; _id: string }[]
+    | { recipes: { name: string; imageSrc: string; _id: string }[] };
+};
+const prepareDumplings = (dumplings: DumplingType) => {
+  if (dumplings.status !== 'fulfilled') {
+    return [];
+  }
+
+  if ('recipes' in dumplings.value) {
+    return dumplings.value.recipes.map(({ _id, imageSrc, name }) => ({
+      _id,
+      imageSrc,
+      name,
+    }));
+  }
+
+  return dumplings.value.map(({ _id, imageSrc, name }) => ({
+    _id,
+    imageSrc,
+    name,
+  }));
+};
+
 const DumplingShopPage = () => {
+  const [dumplings, setDumplings] = useState<{
+    myDumplings: ReturnType<typeof prepareDumplings>;
+    allDumplings: ReturnType<typeof prepareDumplings>;
+  }>({
+    myDumplings: [],
+    allDumplings: [],
+  });
+
+  useEffect(() => {
+    const getDumplings = async () => {
+      const dumplingsPromises = [
+        findMyDumplingRecipes(),
+        listAllDumplingRecipes(),
+      ];
+
+      const [myDumplings, allDumplings] = (await Promise.allSettled(
+        dumplingsPromises,
+      )) as DumplingType[];
+
+      console.log([myDumplings, allDumplings]);
+      setDumplings({
+        myDumplings: prepareDumplings(myDumplings),
+        allDumplings: prepareDumplings(allDumplings),
+      });
+    };
+
+    getDumplings();
+  }, []);
+
+  console.log('Tablice obiektów do wyświetlenia:', dumplings);
+
   return (
     <div>
       <h1>Moje Pierogi</h1>
