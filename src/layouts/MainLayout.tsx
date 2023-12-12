f48bc26fd9eaacd7fbf3e485db351cbd852bf1e1
Adrian <klimasa98@gmail.com>
2023-12-12 21:49:25 +0100

    created recipes component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import HeaderBg from '@components/Button/header';
+import GeneratorForm from '@components/Form/GeneratorForm';
+import Recipes from '@components/Recipes/Recpipes';
+
 
 
 const MainLayout = () => {
@@ -8,6 +11,8 @@ const MainLayout = () => {
       <HeaderBg />
       <section className="px-4 mt-[136px]">
         <Outlet />
+        <GeneratorForm />
+
       </section>
     </main>
   );
