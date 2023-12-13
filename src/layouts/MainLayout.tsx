e36315e887d77f7a1f75965c84014ef5f39a8d27
Adrian <klimasa98@gmail.com>
2023-12-13 12:35:54 +0100

    changeLoaderColor

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,17 +3,14 @@ import HeaderBg from '@components/Button/header';
 import GeneratorForm from '@components/Form/GeneratorForm';
 import Recipes from '@components/Recipes/Recpipes';
 
-
 const MainLayout = () => {
   return (
-
     <main>
       <HeaderBg>Pierogator świąteczny</HeaderBg>
       <section className="mt-[136px] px-4">
-
         <Outlet />
         <GeneratorForm />
-
+        <Recipes />
       </section>
     </main>
   );
